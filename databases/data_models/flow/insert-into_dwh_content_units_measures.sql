beeb1625f6fa913bcc02821284b338f4121a8cd7
marinaloiter <87082130+marinaloiter@users.noreply.github.com>
2021-07-08 12:48:01 +0300

    Update insert-into_dwh_content_units_measures.sql

--- databases/data_models/flow/insert-into_dwh_content_units_measures.sql
+++ databases/data_models/flow/insert-into_dwh_content_units_measures.sql
@@ -2,8 +2,8 @@ truncate table dwh_content_units_measures;
 insert into dwh_content_units_measures
 (
 select event_unit_uid, content_unit_type_name, sum(event_count) as all_events_count,
-count (distinct case when dwh_update_datetime >= NOW() - (10 * interval '1 minute') then event_user_id else null end) as unique_users_last10min_count,
-max(NOW()) as dwh_update_datetime
+count (distinct case when event_start_date >= NOW() AT TIME ZONE '-3:00' - (interval '1 day') then event_user_id else null end) as unique_users_last10min_count,
+max(NOW() AT TIME ZONE '-3:00') as dwh_update_datetime
 from dwh_fact_play_events_by_day_user
 where content_unit_type_name IS NOT NULL
 group by event_unit_uid, content_unit_type_name
